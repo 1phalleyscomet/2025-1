be8d30d5f73186f1dcd4c1168093ea074f781119
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2025-03-17 12:49:54 +0900

    Add files via upload

---
+++ 기초프로그래밍I_2025-1/001.py
@@ -0,0 +1,30 @@
+
+a=int(input("type int number: "))
+b=int(input("type int number: "))
+#c=a+b
+print("sum=",a+b)
+#print("sum=",c)
+#직관적인 변수명 사용 권장
+
+#1주차 퀴즈 문제-1
+a=str(input("enter sentence:"))
+b=str(input("enter sentence:"))
+c=str(input("enter sentence:"))
+#문자열 3개 입력받아 그 문자를 출력
+print(a,b,c)
+#문자열 3개 입력받아 마지막 3글자를 출력
+print(a[-3:],b[-3:],c[-3:])
+
+#1주차 퀴즈 문제-2
+n1=14
+n2=5
+result=n1%n2
+'''
+논리 연산자
+** 제곱근
+/ float
+// int  #음수->내림
+% 나머지
+'''
+print(result)
+
